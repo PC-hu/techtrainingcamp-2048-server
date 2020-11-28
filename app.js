99909b36ff789eaad8ba1821022a25241e960e32
Your Name <you@example.com>
2020-11-28 14:39:45 +0800

    first

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+var game_var={
+  //players:new Array(),
+  players:[],//存放玩家名称
+  numbers:[],//存放玩家对应序列号随机产生
+  scores:[],//存放玩家对应分数
+  rankedscores:[],//存放排序后的分数
+  status:[],//存放玩家状态
+}
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,226 @@
+var express = require('express');
+var ws = require('nodejs-websocket');
+var router = express.Router();
+
+//game_var用于存放使用的变量
+var game_var={
+  players:[],//存放玩家信息（可用于信息发送）
+  names:[],//存放玩家昵称
+  numbers:[],//存放玩家对应序列号随机产生
+  scores:[],//存放玩家对应分数
+  status:[],//存放玩家状态
+}
+
+//game_2048用于存放使用的函数。
+var game_2048 = {
+  //加入新的玩家
+  addPlayer:function(player){
+    if(game_var.players.indexOf(player) != -1){
+      player.sendText(JSON.stringify({
+        type:"loginFail"
+      }));
+      return ;
+    }
+    var num = game_2048.randNumber();
+    player.number = num;
+    game_var.numbers.push(num);
+    game_var.players.push(player);
+    game_var.names.push(player.name);
+    console.log("加人！");
+    //console.log(game_var.players);
+    //增加玩家初始分数0
+    game_var.scores.push(0);
+    //增加玩家初始状态:正在玩
+    game_var.status.push(0);
+    player.sendText(JSON.stringify({
+      type:"loginSuccess",
+      number: player.number,
+    }));
+    var msg = {
+      type:"newPlayer",
+      name: player.name,
+      id: player.number,
+      count: game_var.players.length,
+    };
+    msg = JSON.stringify(msg);
+    game_var.players.forEach(function(current){
+      current.sendText(msg);
+    });
+  },
+
+  //每个玩家产生随机id
+  randNumber:function(){
+    var num = Math.floor(Math.random()*10000%9000 + 1000);
+    while(game_var.numbers.indexOf(num) != -1){
+      num = Math.floor(Math.random()*10000%9000 + 1000);
+    }
+    return num;
+  },
+
+  //移除退出的玩家
+  removePlayer:function(player){
+    var pos = game_var.players.indexOf(player);
+    if(pos == -1)return ;
+    game_var.numbers.splice(game_var.numbers.indexOf(player.number),1);
+    game_var.players.splice(game_var.players.indexOf(player),1);
+    // if(player.isInRoom){
+    //   game_2048.leaveRoom(player);
+    // }
+    player.sendText(JSON.stringify({type:"logoutSuccess"}));
+    var msg = {
+      type:"playerLogout",
+      name: player.name,
+      id: player.number,
+      count: game_var.players.length
+    };
+    msg = JSON.stringify(msg);
+    game_var.players.forEach(function(current){
+      current.sendText(msg);
+    });
+  },
+
+  //分数排序
+  rankdata:function(){
+    temname = [];
+    temscore = [];
+    temstatus = [];
+    for(i = 0;i < game_var.scores.length;i++){
+      if(temscore.length == 0){
+        temscore.push(game_var.scores[i]);
+        temname.push(game_var.names[i]);
+        temstatus.push(game_var.status[i]);
+        continue;
+      }
+      j = 0;
+      while(game_var.scores[i] < temscore[j]){
+        j++;
+      }
+      temscore.splice(j,0,game_var.scores[i]);
+      temname.splice(j,0,game_var.names[i]);
+      temstatus.splice(j,0,game_var.status[i]);
+    }
+    game_var.names = temname;
+    game_var.scores = temscore;
+    game_var.status = temstatus;
+  },
+
+  //对新入数据进行更新
+  updatedata:function(player){
+
+    //对应玩家把响应数据改入数组
+    game_var.scores.splice(game_var.names.indexOf(player.name),1,player.score);
+    game_var.status.splice(game_var.names.indexOf(player.name),1,player.status);
+
+    //这里调用新函数进行排名工作
+    game_2048.rankdata();
+
+    //服务器端向前端发送分数相关数据（格式转换+发送）
+    var dataT=[];
+    for(i = 0;i<game_var.scores.length;i++){
+      var one = {};
+      one.name = game_var.names[i];
+      one.score = game_var.scores[i];
+      one.status = game_var.status[i];
+      dataT.push(one);
+    }
+
+    var msg = {
+      type:"rank",
+      data:dataT,
+    };
+    //console.log("有数据进来！");
+    msg = JSON.stringify(msg);
+    game_var.players.forEach(function(current){
+      current.sendText(msg);
+    });
+  },
+
+  //更新并发送时间
+  sendtime:function(conn){
+    var playtime = 1800;
+    setInterval(function(){
+      playtime--;
+    },1000);
+      var msg = {
+        type:"time",
+        duration: playtime,
+      };
+      msg = JSON.stringify(msg);
+      conn.sendText(msg);
+  },
+
+  //更新聊天信息
+  letschat:function(player,msg){
+    if(game.players.indexOf(player) == -1)return ;
+    var msg = {
+      type:"letschat",
+      name: player.name,
+      str: msg
+    };
+    msg = JSON.stringify(msg);
+    game.players.forEach(function(current){
+      current.sendText(msg);
+    });
+  },
+
+  //更新表情信息
+  letsmakeface:function(player,msg){
+    if(game.players.indexOf(player) == -1)return ;
+    var msg = {
+      type:"letsmakeface",
+      name: player.name,
+      facenum: msg
+    };
+    msg = JSON.stringify(msg);
+    game.players.forEach(function(current){
+      current.sendText(msg);
+    });
+  },
+}
+
+console.log("正在建立websocket");
+
+ws.createServer(function(conn){
+  conn.on("text",function(str){
+    var msg = JSON.parse(str);
+    game_2048.sendtime(conn);
+    if(msg.type == "login"){
+      conn.name = msg.name;
+      game_2048.addPlayer(conn);
+      console.log(conn.name+"["+conn.number+"]",str);
+    } else if(msg.type == "logout"){
+      game_2048.removePlayer(conn);
+      console.log(conn.name+"["+conn.number+"]",str);
+    } else if(msg.type=="data"){
+      conn.name = msg.name;
+      console.log(conn.name);
+      conn.score = msg.score;
+      conn.status = msg.status;
+      game_2048.updatedata(conn);
+      console.log("分数更新");
+    } else if(msg.type=="letschat"){
+      conn.name = msg.name;
+      game_2048.letschat(conn,msg.str);
+      console.log(str);
+    } else if(msg.type=="letsmakeface"){
+      conn.name = msg.name;
+      game_2048.letschat(conn,msg.facenum);
+      console.log(str);
+    }
+  });
+  
+  conn.on("close",function(code,reason){
+    game_2048.removePlayer(conn);
+  });
+  
+  conn.on("error",function(code,reason){
+    game_2048.removePlayer(conn);
+  });
+}).listen(3001);
+
+console.log("websocket建立完成");
+module.exports = function(app){
+  app.get('/2048',function(req,res){
+    res.render('index', { title: '' });
+  });
+};
